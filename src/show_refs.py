0b96e5732fc9f8b4b7a61421f69c482421b6ff67
Jean Carlos Schappo <jeancsbnu@gamil.com>
2026-05-10 16:26:22 -0300

    Ajuste de prompt

--- src/show_refs.py
+++
@@ -1,8 +0,0 @@
-import json, sys
-sys.stdout.reconfigure(encoding='utf-8')
-examples=[json.loads(l) for l in open('datasets/bug_to_user_story.jsonl','r',encoding='utf-8') if l.strip()]
-for i in [12,13,14]:
-    ref = examples[i]['outputs']['reference']
-    print(f'=== Example {i+1} (full) ===')
-    print(ref)
-    print()
